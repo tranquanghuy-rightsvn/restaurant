742472fd142078187c5e799269d315528dade573
Minhhoang112331 <minhnhpd02469@gmail.com>
2022-09-07 20:30:55 +0700

    Task 5: List Restaurant

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,9 +1,4 @@
 class Admin::AdminController < ActionController::Base
   layout "admin"
-  before_action :check_admin
-
-  def check_admin
-    return if current_user.present? && current_user.admin?
-    redirect_to root_path
-  end
+  before_action :authenticate_user!
 end
